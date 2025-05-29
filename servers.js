0db4ca03ca2581f2c889494ceebe2333f9803b45
uptempobarbie <amhill_101@hotmail.com>
2025-05-29 12:35:09 +1000

    Update servers.js

--- servers.js
+++ servers.js
@@ -9,12 +9,7 @@ app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
-
-mongoose.connect(process.env.MONGODB_URI);
-                
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/yourdb");
 
 // User schema and model
 const UserSchema = new mongoose.Schema({
