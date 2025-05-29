0a15ea6e06ad3b4b42f0c4b75f6d4e09148d3b32
uptempobarbie <amhill_101@hotmail.com>
2025-05-29 12:25:29 +1000

    Update servers.js

--- servers.js
+++ servers.js
@@ -9,8 +9,7 @@ app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
-// Connect to MongoDB
-// Make sure it looks something like this:
+
 mongoose.connect(process.env.MONGODB_URI 
                 
   useNewUrlParser: true,
